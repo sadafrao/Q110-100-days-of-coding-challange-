c43f36ca88a13999cd4e079eb8b724c33bb3177f
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 16:39:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Q110
+// this fuction assign grades (A,B,C,D,F) based on student score
+function assignGrade(score) {
+    if (score >= 90) {
+        return "A";
+    }
+    else if (score >= 80) {
+        return "B";
+    }
+    else if (score >= 70) {
+        return "C";
+    }
+    else if (score >= 60) {
+        return "D";
+    }
+    else {
+        return "F";
+    }
+}
+console.log(assignGrade(95));
---
+++ index.ts
@@ -0,0 +1,16 @@
+// Q110
+// this fuction assign grades (A,B,C,D,F) based on student score
+function assignGrade(score: number): string {
+    if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+console.log(assignGrade(95)); 
